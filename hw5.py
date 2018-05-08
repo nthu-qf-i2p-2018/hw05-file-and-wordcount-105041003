d23c227f798111303d9898e5e80f74d9e583bac2
105041003 <37080305+105041003@users.noreply.github.com>
2018-05-08 17:55:49 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -21,7 +21,7 @@ def main(filename):
     for line in lines:
         words = line.split()
         for word in words:
-            word.strip(string.punctuation)
+            word = word.strip(string.punctuation)
             if word:
                 all_words.append(word)
     df = set(all_words)
@@ -30,26 +30,23 @@ def main(filename):
         list1.append(w)
         list1.append(counter)
     
-    with open("wordcount.csv","w+",newline='') as csv_file:
+    with open("wordcount.csv","w",newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
         for k in range(0,len(list1),2):
             w=list1[k]
             counter=list1[k+1]
             writer.writerow([w,counter])
-        csv_file.close()
 
     dictionary=dict()
 
     for t in range(0,len(list1),2):
         dictionary[list1[t]]=list1[t+1]
-    with open("wordcount.json","w+",newline='') as json_file:
+    with open("wordcount.json","w",newline='') as json_file:
         json.dump(dictionary,json_file)
-        json_file.close()
         
     with open("wordcount.pkl","wb") as pickle_file:
         pickle.dump(dictionary,pickle_file)
-        pickle_file.close()
         
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
